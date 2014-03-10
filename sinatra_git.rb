fefdab63625e4e2277e0d6e35aec90851889986b
Ryan Osten <ryan@classvantage.com>
2014-03-10 19:53:29 -0400

    add list

--- sinatra_git.rb
+++ sinatra_git.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+	'
 	<h1 id="heading1">"This is my heading"</h1>
 	<p>"Hello World!"</p>
+	<ol>
+		<li>"get office"</li>
+		<li>"hire people"</li>
+		<li>"get rich"</li>
+	</ol>
+	'
 end
