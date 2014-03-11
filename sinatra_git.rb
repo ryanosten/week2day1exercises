4560d1c606a33f5e6eeb21a62c62143564406d94
Ryan Osten <ryan@classvantage.com>
2014-03-10 20:34:22 -0400

    strikethrough

--- sinatra_git.rb
+++ sinatra_git.rb
@@ -5,11 +5,12 @@ get '/' do
 	'
 	<style>
 		p{color: blue;}
+		.done{text-decoration:line-through}
 	</style>
 	<h1 id="heading1">This is my heading</h1>
 	<p>Hello World!</p>
 	<ol>
-		<li>get office</li>
+		<li class="done">get office</li>
 		<li>hire people</li>
 		<li>get rich</li>
 	</ol>
