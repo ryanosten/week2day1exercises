ae53e46c15d9804a33576c06254750ef56e07c47
Ryan Osten <ryan@classvantage.com>
2014-03-10 20:55:20 -0400

    erb

--- app.rb
+++ app.rb
@@ -2,17 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	'
-	<style>
-		p{color: blue;}
-		.done{text-decoration:line-through}
-	</style>
-	<h1 id="heading1">This is my heading</h1>
-	<p>Hello World!</p>
-	<ol>
-		<li class="done">get office</li>
-		<li>hire people</li>
-		<li>get rich</li>
-	</ol>
-	'
+	erb :index
 end
+
+
+
