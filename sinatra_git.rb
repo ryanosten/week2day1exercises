56417a7f7112d185912e5d4917fe93135727398e
Ryan Osten <ryan@classvantage.com>
2014-03-10 20:14:13 -0400

    quote fix

--- sinatra_git.rb
+++ sinatra_git.rb
@@ -3,12 +3,12 @@ require 'sinatra/reloader'
 
 get '/' do
 	'
-	<h1 id="heading1">"This is my heading"</h1>
-	<p>"Hello World!"</p>
+	<h1 id="heading1">This is my heading</h1>
+	<p>Hello World!</p>
 	<ol>
-		<li>"get office"</li>
-		<li>"hire people"</li>
-		<li>"get rich"</li>
+		<li>get office</li>
+		<li>hire people</li>
+		<li>get rich</li>
 	</ol>
 	'
 end
